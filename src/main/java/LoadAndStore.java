5d898e9a9a9e6d8288d9a4755e87b9b9346662cc
AndrewAscone <andrew.ascone@gmail.com>
2022-11-04 19:00:37 -0400

    Completed LoadAndStore, made tests in LoadAndStoreTest and all pass

--- src/main/java/LoadAndStore.java
+++ src/main/java/LoadAndStore.java
@@ -6,11 +6,14 @@ import java.util.ArrayList;
 
 public class LoadAndStore {
 
-    public int[] loadIntArrayFromFile(String filename) {
+    public int[] loadIntArrayFromFile(String filename) { //file > arraylist > array
         ArrayList<Integer> arrList = loadIntegerArrayListFromFile(filename);
+
         int[] arr = new int[arrList.size()];
+
         for (int i = 0; i < arrList.size(); i++)
             arr[i] = arrList.get(i);
+
         return arr;
     }
 
@@ -22,7 +25,10 @@ public class LoadAndStore {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+
         String line ="";
+
         ArrayList<Integer> result = new ArrayList<Integer>();
 
         while (true) {
@@ -45,15 +51,87 @@ public class LoadAndStore {
 
     // use testDoubleData2.txt to test
 
-    public double[] loaddoubleArrayFromFile(String filename) { return new double[]{}; }
+    public double[] loaddoubleArrayFromFile(String filename) {
+        ArrayList<Double> arrList = loadDoubleArrayListFromFile(filename);
 
-    public ArrayList<Double> loadDoubleArrayListFromFile(String filename) { return null; }
+        double[] arr = new double[arrList.size()];
+
+        for(int i = 0; i < arrList.size(); i++)
+            arr[i] = arrList.get(i);
+
+        return arr;
+    }
+
+    public ArrayList<Double> loadDoubleArrayListFromFile(String filename) {
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        String line = "";
+
+        ArrayList<Double> result = new ArrayList<Double>();
+
+        while (true) {
+            try {
+                if ((line = br.readLine()) == null) break;
+                if(line.startsWith("//")) continue;
+                Double number = Double.parseDouble(line);
+                result.add(number);
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
 
     // use testStringData3.txt to test
 
-    public String[] loadStringArrayFromFile(String filename) { return null; }
+    public String[] loadStringArrayFromFile(String filename) {
+        ArrayList<String> arrList = loadStringArrayListFromFile(filename);
 
-    public ArrayList<String> loadStringArrayListFromFile(String filename) { return null; }
+        String[] arr = new String[arrList.size()];
+
+        for (int i = 0; i < arrList.size(); i++)
+            arr[i] = arrList.get(i);
+
+        return arr;
+    }
+
+    public ArrayList<String> loadStringArrayListFromFile(String filename) {
+        // Creating an object of BufferedReader class
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        String line ="";
+
+        ArrayList<String> result = new ArrayList<String>();
+
+        while (true) {
+            try {
+                if ((line = br.readLine()) == null) break; // break loop at end of file
+                if (line.startsWith("//")) continue; // ignore "//" comment lines
+                result.add(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
 
     //
     // Finally:
--- src/test/java/LoadAndStoreTest.java
+++ src/test/java/LoadAndStoreTest.java
@@ -2,11 +2,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadAndStoreTest {
 
     // write all these out too.
+    String intFile = "/Users/andrew/Projects/ArraysAndLists/testIntegerData1.txt";
+
+    String doubleFile = "/Users/andrew/Projects/ArraysAndLists/testDoubleData2.txt";
+
+    String stringFile = "/Users/andrew/Projects/ArraysAndLists/testStringData3.txt";
 
     @BeforeEach
     void setUp() {
@@ -18,31 +26,49 @@ class LoadAndStoreTest {
 
     @Test
     void loadIntArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4, 5, 6, 11, 13, 8, 9, 7, 14, 18]";
+        String actual = Arrays.toString(check.loadIntArrayFromFile(intFile));
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadIntegerArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4, 5, 6, 11, 13, 8, 9, 7, 14, 18]";
+        String actual = String.valueOf(check.loadIntegerArrayListFromFile(intFile));
+        assertEquals(expected, actual);
     }
 
     @Test
     void loaddoubleArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4.4, 5.5, 6.34, 11.0001, 13.004, 8.7, 9.9706, 7.4532, 14.5, 18.00023132123]";
+        String actual = Arrays.toString(check.loaddoubleArrayFromFile(doubleFile));
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadDoubleArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4.4, 5.5, 6.34, 11.0001, 13.004, 8.7, 9.9706, 7.4532, 14.5, 18.00023132123]";
+        String actual = String.valueOf(check.loadDoubleArrayListFromFile(doubleFile));
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!]";
+        String actual = Arrays.toString(check.loadStringArrayFromFile(stringFile));
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayListFromFile() {
-        assertEquals(null, "");
+        LoadAndStore check = new LoadAndStore();
+        String expected = "[4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!]";
+        String actual = String.valueOf(check.loadStringArrayListFromFile(stringFile));
+        assertEquals(expected, actual);
     }
 }
