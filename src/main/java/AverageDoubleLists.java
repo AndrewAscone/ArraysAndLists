5d1bc628f3f9485132f573e9a3d0a4105a395152
AndrewAscone <andrew.ascone@gmail.com>
2022-11-04 13:39:39 -0400

    AverageDoubleLists complete, made tests in AverageDoubleListsTest and all pass

--- src/main/java/AverageDoubleLists.java
+++ src/main/java/AverageDoubleLists.java
@@ -3,14 +3,28 @@ import java.util.ArrayList;
 public class AverageDoubleLists {
 
     // write out these too
-    public Double count(ArrayList<Double> aa) {
-        return null;
+    public int count(ArrayList<Double> aa) {
+        return aa.size();
     }
     public Double sum(ArrayList<Double> aa) {
-        return null;
+        double sum = 0;
+        for (int i = 0; i < aa.size(); i++){
+            sum += aa.get(i);
+        }
+
+        return sum;
     }
     public Double average(ArrayList<Double> aa) {
-        return null;
+        double sum = 0.0;
+
+        if(aa.size() != 0){
+            for(int i = 0; i < aa.size(); i++){
+                sum += aa.get(i);
+            }
+            return Math.round((sum/aa.size()) * 10.0)/10.0;
+        }
+
+        return 0.0;
     }
 
 }
--- src/test/java/AverageDoubleListsTest.java
+++ src/test/java/AverageDoubleListsTest.java
@@ -1,21 +1,90 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AverageDoubleListsTest {
 
     @Test
     void count() {
-        assertEquals(null, "");
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(1.1, 2.2, 3.3));
+        double expected = 3.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.count(a));
+    }
+
+    @Test
+    void count2(){
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(0.0, 2.3, 4.1, 0.0, 1.1, 0.0, 0.0));
+        double expected = 7.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.count(a));
+    }
+
+    @Test
+    void count3(){
+        ArrayList<Double> a = new ArrayList<>();
+        double expected = 0.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.count(a));
     }
 
     @Test
     void sum() {
-        assertEquals(null, "");
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(1.1, 2.2, 3.3));
+        double expected = 6.6;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.sum(a));
+    }
+
+    @Test
+    void sum2(){
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(0.0, 0.0, 2.5, 5.2, 3.3, 0.0));
+        double expected = 11.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.sum(a));
+    }
+
+    @Test
+    void sum3(){
+        ArrayList<Double> a = new ArrayList<>();
+        double expected = 0.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.sum(a));
     }
 
     @Test
     void average() {
-        assertEquals(null, "");
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(1.1, 2.2, 3.3));
+        double expected = 2.2;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.average(a));
+    }
+
+    @Test
+    void average2(){
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(0.0, 1.3, 0.0, 7.8, 3.4));
+        double expected = 2.5;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.average(a));
+    }
+
+    @Test
+    void average3(){
+        ArrayList<Double> a = new ArrayList<>();
+        double expected = 0.0;
+        AverageDoubleLists obj = new AverageDoubleLists();
+
+        assertEquals(expected, obj.average(a));
     }
 }
