ea8f4f185e732c6290b43f05bf69e2d0bdf4c5a5
AndrewAscone <andrew.ascone@gmail.com>
2022-11-04 11:48:07 -0400

    AverageIntArrays complete, AverageArraysTest passes

--- src/main/java/AverageIntArrays.java
+++ src/main/java/AverageIntArrays.java
@@ -3,14 +3,27 @@ public class AverageIntArrays {
 // Complete these methods
 
     public int count(int[] aa) {
-        return 0;
+        return aa.length;
     }
 
     public int sum(int[] aa) {
-        return 0;
+        int sum = 0;
+        for(int i = 0; i < aa.length; i++){
+            sum += aa[i];
+        }
+        return sum;
     }
 
     public int average(int[] aa) {
+        int sum = 0;
+        int average = 0;
+
+        if(aa.length != 0) {
+            for (int i = 0; i < aa.length; i++) {
+                sum += aa[i];
+            }
+            return sum/aa.length;
+        }
         return 0;
     }
 
